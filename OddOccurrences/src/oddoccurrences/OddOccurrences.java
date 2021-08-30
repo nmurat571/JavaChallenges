a505b0f4224c189a2145661361441b05802bddcc
nmurat571 <muratnatalia10@gmail.com>
2021-08-30 18:51:51 -0300

    metodo solucion() agregado en OddOccurences

--- OddOccurrences/src/oddoccurrences/OddOccurrences.java
+++ OddOccurrences/src/oddoccurrences/OddOccurrences.java
@@ -11,11 +11,56 @@ package oddoccurrences;
  */
 public class OddOccurrences {
 
+    public int solution(int[] A) {
+
+        int[] check = new int[A.length];
+        boolean checked;
+        int c;
+        int odd = 0;
+
+        for (int i = 0; i < A.length; i++) {
+
+            c = 0;
+            checked = false;
+            
+            for (int k = 0; k < check.length; k++) {
+                if (A[i] == check[k]) {
+                    checked = true;
+                    break;
+                }else if(check[k]==0){
+                    break;
+                }
+            }
+
+            if (!checked) {
+                for (int ctrl : A) {
+                    if (A[i] == ctrl) {
+                        c++;
+                    }
+                }
+
+                if (c % 2 == 0) { //even element
+                    check[i] = A[i];
+                } else { //odd element
+                    odd = A[i];
+                }
+            } 
+
+        }
+
+        return odd;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int[] A = {2,500,400,500,400,2,307};
+        OddOccurrences oc = new OddOccurrences();
+        int o = oc.solution(A);
+        System.out.println("Unpaired: " + o);
     }
-    
+
 }
